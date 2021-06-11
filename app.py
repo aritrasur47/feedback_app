c0fbbc6b83a04ce2e62ea2d2246c2fba178f4af1
maila <mail.aritrasur@gmail.com>
2021-06-11 21:21:35 +0530

    Updated uri section commands to replace postgres to postgresql

--- app.py
+++ app.py
@@ -2,17 +2,23 @@ from enum import unique
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from send_mail import send_mail
+import os
 
 app = Flask(__name__)
 
 ENV = 'prod'
+
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:TestBest#4@localhost/lexus'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zagvzyffbajsvv:e7182d8746da358b6378ab2d31022a66a9533959b4a88f81b8075e19627caf67@ec2-34-193-112-164.compute-1.amazonaws.com:5432/dbublr2nmpguap'
+    app.config['SQLALCHEMY_DATABASE_URI'] = uri
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zagvzyffbajsvv:e7182d8746da358b6378ab2d31022a66a9533959b4a88f81b8075e19627caf67@ec2-34-193-112-164.compute-1.amazonaws.com:5432/dbublr2nmpguap'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
